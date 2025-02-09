f9fd0bd3855b1efdb7251827577dd7b8f79ef76d
Vogel-Git <61388265+Vogel-Git@users.noreply.github.com>
2025-02-09 20:21:24 +0100

    Update helper.h

--- config/include/zmk-helpers/helper.h
+++ config/include/zmk-helpers/helper.h
@@ -209,11 +209,6 @@
     UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
     UC_MODMORPH(name, &name ## _lower, &name ## _upper)
 
-#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
-    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
-    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
-    UC_MODMORPH(name, &name ## _lower, &name ## _upper)
-
 /* ZMK_APPLY_MATRIX_TRANSFORM */
 
 #define ZMK_APPLY_MATRIX_TRANSFORM(transform) \
